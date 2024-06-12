48d78b6f04593e037cec36bfe8d2c26bfd5ad39d
kok shen <kokshen3414@gmail.com>
2024-06-13 01:37:16 +0800

    button customisation

--- Guess_game/app/src/main/java/my/edu/utar/guess_game/MainActivity.java
+++ Guess_game/app/src/main/java/my/edu/utar/guess_game/MainActivity.java
@@ -3,12 +3,73 @@ package my.edu.utar.guess_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonStart,buttonLeaderboard,buttonExit;
+    Animation scaleUp,scaleDown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.button_start);
+        buttonLeaderboard = findViewById(R.id.leaderboard);
+        buttonExit = findViewById(R.id.exit);
+
+        scaleUp = AnimationUtils.loadAnimation(this,R.anim.scale_up);
+        scaleDown = AnimationUtils.loadAnimation(this,R.anim.scale_down);
+
+        buttonStart.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    buttonStart.startAnimation(scaleUp);
+                }
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    buttonStart.startAnimation(scaleDown);
+                }
+
+
+                return false;
+            }
+        });
+
+
+        buttonLeaderboard.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    buttonLeaderboard.startAnimation(scaleUp);
+                }
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    buttonLeaderboard.startAnimation(scaleDown);
+                }
+
+
+                return false;
+            }
+        });
+
+        buttonExit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    buttonExit.startAnimation(scaleUp);
+                }
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    buttonExit.startAnimation(scaleDown);
+                }
+
+
+                return false;
+            }
+        });
+
+
     }
 }
