605e73f3d1ac9d84cd3cb807133124c473eaa834
kok shen <kokshen3414@gmail.com>
2024-06-18 20:06:47 +0800

    updated solution

--- Guess_game/app/src/main/java/my/edu/utar/guess_game/compare_number.java
+++ Guess_game/app/src/main/java/my/edu/utar/guess_game/compare_number.java
@@ -17,6 +17,9 @@ public class compare_number extends AppCompatActivity {
 
     Button btn;
 
+    TextView textLevel;
+    int level = 0;
+
 //    below is part of the activity lifecycle method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,11 @@ public class compare_number extends AppCompatActivity {
         txt3 = findViewById(R.id.txt3);
         btn = findViewById(R.id.btn);
 
+//        incremental level logic
+        textLevel = findViewById(R.id.textQuestionNumber);
+        textLevel.setText("Q : "+level+" / 10");
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,26 +45,41 @@ public class compare_number extends AppCompatActivity {
                 txt1.setText(Integer.toString(val));
                 txt2.setText(Integer.toString(val2));
 
-//                txt3.setText("Hi");
-//                txt3.setText(val.equals(val2));
-                txt3.setText(Integer.compare(val,val2));
+//                we would call our compare method, in the setText part
+//                txt3.setText(String.valueOf(Integer.compare(val,val2)));
+//                for Integer.compare method, if (x==y) return 0
+//                for Integer.compare method, if (x<y) return -1
+//                for Integer.compare method, if (x>y) return 1
+
+
+                txt3.setText(equal_num(val,val2));
+
+//                experimental code
+//                the logic is to check if id/txt3 is "Not equal" or "Both numbers are equal"
+//                the logic i want to test, is like onClick of a button then then we generate a new level?
+                if(txt3.getText().equals("Both numbers are equal")){
+                    level = level + 1;
+                    textLevel.setText("Q : "+level+" / 10");
+                } else if (txt3.getText().equals("Not equal")) {
+                    level = level+1;
+                    textLevel.setText("Q : "+level+" / 10");
+                }
+
             }
         });
 
     }
 
-//    public boolean equals(Integer num) {
-//        if (obj instanceof Integer) {
-//            return value == ((Integer)obj).intValue();
-//        }
-//        return false;
-//    }
+    public String equal_num(int a, int b){
+        if(a == b){
+            return ("Both numbers are equal");
+        }
+        else {
+            return ("Not equal");
+        }
+    }
 
-//    public boolean equals(Object obj) {
-//        if (obj instanceof Integer) {
-//            return value == ((Integer)obj).intValue();
-//        }
-//        return false;
-//    }
+
+
 
 }
